5b25758b9dedd1899380f62869b7ef2d07ebca1c
binyoma <innocentbinyoma@pm.me>
2024-07-22 08:55:45 +0200

    denying access

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Pagerfanta\Doctrine\ORM\QueryAdapter;
 use Pagerfanta\Pagerfanta;
 use Psr\Log\LoggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -45,9 +46,11 @@ class QuestionController extends AbstractController
 
     /**
      * @Route("/questions/new")
+     * @isGranted("ROLE_USER")
      */
     public function new()
     {
+        
         return new Response('Sounds like a GREAT feature for V2!');
     }
 
