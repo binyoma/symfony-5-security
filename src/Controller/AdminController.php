71e807013ceb9756fe8b04f36440f2c5aacfe71a
binyoma <innocentbinyoma@pm.me>
2024-07-22 11:54:01 +0200

    fetching user object

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -54,4 +54,11 @@ class AdminController extends AbstractController
             'chart2' => $chart2,
         ]);
     }
+    /**
+     * @Route("/admin/login")
+     */
+    public function adminLogin(){
+        return new Response('Pretend admin login page that should be public');
+
+    }
 }
